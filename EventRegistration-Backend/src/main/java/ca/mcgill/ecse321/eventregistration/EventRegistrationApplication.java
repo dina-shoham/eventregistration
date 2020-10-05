b706c4e86c1e379d35bc5870e73ee4d93f0e12db
Dina Shoham <dina.shoham@gmail.com>
2020-10-05 11:32:43 -0400

    restarting again lol

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplication.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplication.java
@@ -1,7 +1,7 @@
 package ca.mcgill.ecse321.eventregistration;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
